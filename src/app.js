5937d17d21e3e448247f6ec3dd2594674747ef84
majg1987 <majg1987@hotmail.com>
2022-05-09 19:19:59 +0000

    generate-excuse terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function excuse() {
+    const who = ["The dog", "My grandma", "His turtle", "My bird"];
+    const action = ["ate", "peed", "crushed", "broke"];
+    const what = ["my homework", "the keys", "the car"];
+    const when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    return (
+      who[aleatorio(who)] +
+      " " +
+      action[aleatorio(action)] +
+      " " +
+      what[aleatorio(what)] +
+      " " +
+      when[aleatorio(when)]
+    );
+  }
+
+  function aleatorio(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+  document.querySelector("#excuse").innerHTML = excuse();
 };
